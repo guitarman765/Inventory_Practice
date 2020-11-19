6a47d0e1f8f8aa2272f43424ceb8c2290b62eb85
guitarman765 <74016487+guitarman765@users.noreply.github.com>
2020-11-19 12:14:10 -0500

    Added pickle notes for saving inventory after program close.

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 # Inventory Tracker.
+# Inventory tracker, database will be a text file, security concerns for text file, public inventory list instead of private.
+# Can also create an ini file for dictonary saving but I dont know how to do that yet so using text files for now.
+
+# import pickle
+# Save a dictionary into a pickle file.
+# favorite_color = { "lion": "yellow", "kitty": "red" }
+# pickle.dump( favorite_color, open( "save.p", "wb" ) )
+
+#
+# Load the dictionary back from the pickle file.
+# favorite_color = pickle.load( open( "save.p", "rb" ) )
 
 
+
+# The inventory stored as a dictionary.
+inventory_dict = {}
 def user_choice(user_input=int(input("Type Choice: "))):
     if user_input == 1:
         return print(1)
