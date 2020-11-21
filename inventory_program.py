c9ac298f9f4979d620ed3c7d491ef24e10efa9f9
guitarman765 <74016487+guitarman765@users.noreply.github.com>
2020-11-20 20:00:59 -0500

    Updating location update function.

--- inventory_program.py
+++ inventory_program.py
@@ -2,19 +2,9 @@
 import sqlite3
 
 
-def create_db():
-    # Create Database.
-    connection = sqlite3.connect('inventory_db')
-
-    # Define connection and cursor.
-    cursor = connection.cursor()
-
-    # Create inventory table command.
-    create_table = """CREATE TABLE IF NOT EXISTS
-    inventory (item_num INTEGER PRIMARY KEY, location TEXT)"""
-
-    # Create cursor and create inventory table.
-    cursor.execute(create_table)
+def c_comm():
+    connection.commit()
+    connection.close()
 
 
 def user_choice():
@@ -43,29 +33,46 @@ def find_location():
     return print("Find Location")
 
 
+# FIX ME!
 def location_update():
-    return print("Update Location")
+    ("UPDATE inventory SET location = (?) WHERE item_id = (?)", (?, ?))
 
-
+# FIX ME!
 def delete_item():
-    return print("Delete Item")
+    del_item_num = input("Enter Item Number To Add To Inventory: ")
+    del_item_location = input("Enter Item Location Of Item Added To Inventory: ")
+    verify_del_input = input(f"Are You Sure You Want To Add Item: {del_item_num} To Location: {del_item_location} Y/N: ")
+    if verify_del_input == "Y" or "y":
+        cursor.execute("DELETE FROM inventory WHERE ? = ?", (del_item_num, del_item_location))
+        c_comm()
+    else:
+        pass
 
 
-# FIX ME
+
 def add_item():
     add_item_num = input("Enter Item Number To Add To Inventory: ")
     add_item_location = input("Enter Item Location Of Item Added To Inventory: ")
-    verify_input = input(
-        "Are You Sure You Want To Add Item: {} To Location: {} Y/N:".format(add_item_num, add_item_location))
+    verify_input = input(f"Are You Sure You Want To Add Item: {add_item_num} To Location: {add_item_location} Y/N: ")
     if verify_input == "Y" or "y":
-        inventory_dict.update({add_item_num: add_item_location})
+        cursor.execute("INSERT INTO inventory VALUES (?,?)", (add_item_num, add_item_location))
+        c_comm()
     else:
-        add_item()
+        pass
 
 
-# Start of the program.
-create_db()
+# Create Database.
+connection = sqlite3.connect('inventory_db')
+
+# Define connection and cursor.
+cursor = connection.cursor()
+
+# Create inventory table command.
+create_table = """CREATE TABLE IF NOT EXISTS
+inventory (item_num INTEGER PRIMARY KEY, location TEXT)"""
+
+# Create cursor and create inventory table.
+cursor.execute(create_table)
+
 user_choice()
-
-
-
+c_comm()
