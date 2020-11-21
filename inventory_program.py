e9006e2521e9a273a9152e4c28ca65bebf39e9dc
guitarman765 <74016487+guitarman765@users.noreply.github.com>
2020-11-20 21:46:58 -0500

    Updating add item function.

--- inventory_program.py
+++ inventory_program.py
@@ -12,6 +12,7 @@ def user_choice():
     print("2: Change Item Location In Inventory")
     print("3: Delete Item From Inventory")
     print("4: Add Item To Inventory")
+    print("5: View Inventory")
 
     # Get user input.
     user_input = int(input("Type Choice: "))
@@ -25,6 +26,8 @@ def user_choice():
         return delete_item()
     if user_input == 4:
         return add_item()
+    if user_input == 5:
+        return view_inventory()
     else:
         return print("Not a choice, please try again.")
 
@@ -35,34 +38,42 @@ def find_location():
 
 # FIX ME!
 def location_update():
-    ("UPDATE inventory SET location = (?) WHERE item_id = (?)", (?, ?))
+    pass
+# ("UPDATE inventory SET location = (?) WHERE item_id = (?)", (?, ?))
+
 
 # FIX ME!
 def delete_item():
-    del_item_num = input("Enter Item Number To Add To Inventory: ")
-    del_item_location = input("Enter Item Location Of Item Added To Inventory: ")
-    verify_del_input = input(f"Are You Sure You Want To Add Item: {del_item_num} To Location: {del_item_location} Y/N: ")
+    del_item_num = input("Enter Item Number To Delete From Inventory: ")
+    verify_del_input = input(f"Are You Sure You Want To Delete Item: {del_item_num} From The Inventory? Y/N: ")
     if verify_del_input == "Y" or "y":
-        cursor.execute("DELETE FROM inventory WHERE ? = ?", (del_item_num, del_item_location))
+        cursor.execute("DELETE FROM inventory WHERE item_id = (?)", del_item_num)
         c_comm()
     else:
         pass
 
 
+def view_inventory():
+    cursor.execute("SELECT * FROM inventory")
+    results = cursor.fetchall()
+    print(results)
 
+# FIX ME
 def add_item():
     add_item_num = input("Enter Item Number To Add To Inventory: ")
     add_item_location = input("Enter Item Location Of Item Added To Inventory: ")
     verify_input = input(f"Are You Sure You Want To Add Item: {add_item_num} To Location: {add_item_location} Y/N: ")
     if verify_input == "Y" or "y":
+        connection = sqlite3.connect('inventory_db')
+        cursor = connection.cursor()
         cursor.execute("INSERT INTO inventory VALUES (?,?)", (add_item_num, add_item_location))
         c_comm()
     else:
         pass
 
 
-# Create Database.
 connection = sqlite3.connect('inventory_db')
+
 
 # Define connection and cursor.
 cursor = connection.cursor()
@@ -76,3 +87,4 @@ cursor.execute(create_table)
 
 user_choice()
 c_comm()
+
